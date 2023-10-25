7dccdc1a377ee82373b017bd5dbb85ae24bcb866
Varshaaa25 <varshagowramma2003@gmail.com>
2023-10-25 19:14:47 +0530

    my commit

---
+++ index.js
@@ -0,0 +1,33 @@
+//variables
+let name='varsha';
+console.log(name);
+prompt('enter name');
+alert(name);
+
+//constant
+const interestRate=0.3;
+console.log(interestRate);
+
+
+//datatypes
+let n='vamshi';//string
+let age=20;//number literal
+let isApproved=true;//boolean literal
+let fName=undefined;
+let selectedColor=null;
+
+//referance types
+//object literal
+let person={
+    adress:'hujigfc',
+    phNo:203456788
+
+};
+//change or access members of objects using 
+//1.dot operator
+person.adress='ghut';
+//2.bracket notaation-dynamic 
+person['adress']='ghunbvff';
+
+console.log(person.adress);
+alert(person);
